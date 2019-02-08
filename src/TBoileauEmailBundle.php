931a48061d720f938e62213a99c4be7a9c6b7a4e
Thomas Boileau <t-boileau@email.com>
2019-02-08 10:14:00 +0100

    Add bundle class

---
+++ src/TBoileauEmailBundle.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * (c) Thomas Boileau <t-boileau@email.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace TBoileau\Bundle\EmailBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class TBoileauEmailBundle
+ *
+ * @package TBoileau\Bundle\EmailBundle
+ * @author Thomas Boileau <t-boileau@email.com>
+ * @codeCoverageIgnore
+ */
+class TBoileauEmailBundle extends Bundle
+{
+
+}
